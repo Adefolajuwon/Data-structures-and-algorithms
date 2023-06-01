6b52d502cb367d54e34c6b9018c24bcbc9efa87e
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-06-01 23:35:05 +0100

    change

--- tut.py
+++ tut.py
@@ -1,9 +1,17 @@
-from math import *
-print('hello world')
-cat = "kain"
-age = 6
-print("my cat name is" + cat + ",")
+# from math import *
+# print('hello world')
+# cat = "kain"
+# age = 6
+# print("my cat name is" + cat + ",")
 
 
-def sayhi():
-    print('hiiiiii')
+# def sayhi():
+#     print('hiiiiii')
+
+
+monthConvert = {
+    'jan': 'january',
+    'feb': 'febuarary'
+}
+print(monthConvert.get('jan', 'invalis'))
+# print('food')
