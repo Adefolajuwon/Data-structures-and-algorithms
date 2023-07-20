4a51f2bff3f6ee370766a2e97d0630ecd92b06e9
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-07-20 14:17:00 +0900

    change

--- DSA/binary_search.py
+++ DSA/binary_search.py
@@ -18,7 +18,7 @@ def locate_card(cards, query):
 
 
 test = []
-
+# ok
 test.append({
     'input': {
         'cards': [10, 9, 8, 7, 6, 5, 4, 3, 2, 2, 1],
@@ -33,3 +33,54 @@ test.append({
     'output': 0
 })
 evaluate_test_case(locate_card, test[0])
+
+# leetcode
+
+
+def binary_search(lo, hi, condition):
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        result = condition(mid)
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return -1
+
+
+def first_position(nums, target):
+    def condition(mid):
+        if nums[mid] == target:
+            if mid > 0 and nums[mid - 1] == target:
+                return 'left'
+            return 'found'
+        elif nums[mid] < target:
+            return 'right'
+        else:
+            return 'left'
+    return binary_search(0, len(nums)-1, condition)
+
+
+def last_position(nums, target):
+    def condition(mid):
+        if nums[mid] == target:
+            if mid < len(nums)-1 and nums[mid+1] == target:
+                return 'right'
+            return 'found'
+        elif nums[mid] < target:
+            return 'right'
+        elif nums[mid] > target:
+            return 'left'
+    return binary_search(0, len(nums)-1, condition)
+
+
+def first_and_last_position(nums, target):
+    return first_position(nums, target), last_position(nums, target)
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        return first_and_last_position(nums, target)
+# end
