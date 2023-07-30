f229935930b296d840d5ffe64a2906289ecd759e
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-07-30 21:25:19 +0100

    change

--- DSA/binary.tree.py
+++ DSA/binary.tree.py
@@ -5,28 +5,16 @@ class TreeNode:
         self.right = None
 
 
-node0 = TreeNode(2)
-node1 = TreeNode(3)
-node2 = TreeNode(1)
-node3 = TreeNode(5)
-node4 = TreeNode(3)
-node5 = TreeNode(7)
-node6 = TreeNode(4)
-node7 = TreeNode(6)
-node8 = TreeNode(8)
-
-node0.left = node1
-node1.left = node2
-node0.right = node3
-node3.left = node4
-node3.right = node5
-print(node3.key)
+tree_tuple = ()
 
 
-# node0 = TreeNode(3)
-# node1 = TreeNode(4)
-# node2 = TreeNode(5)
-# # print(node0.key)
-# node0.left = node1
-# node0.right = node2
-# tree = node0
+def parse_tuple(data):
+    if isinstance(data, tuple) and len(data) == 3:
+        node = TreeNode(data[1])
+        node.left = parse_tuple(data[0])
+        node.right = parse_tuple(data[2])
+    elif data is None:
+        return 0
+    else:
+        node = TreeNode(data)
+    return node
