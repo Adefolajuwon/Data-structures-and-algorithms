0b4a5eee1c2ba998d51fe25295f4b2bc83f0602f
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-08-10 17:26:44 +0100

    change

---
+++ DSA/arrays.py
@@ -0,0 +1,11 @@
+months = [2200, 2000, 2600, 2130, 2190]
+jan = months[0]
+feb = months[1]
+total = feb-jan
+print(total)
+
+for i in months:
+    if (i == 2000):
+        print(i)
+    else:
+        print('non')
