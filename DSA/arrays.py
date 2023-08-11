1580f12479d97715b0354b4ef2662f830264f412
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-08-11 09:53:46 +0100

    change

--- DSA/arrays.py
+++ DSA/arrays.py
@@ -1,11 +1,13 @@
-months = [2200, 2000, 2600, 2130, 2190]
-jan = months[0]
-feb = months[1]
-total = feb-jan
-print(total)
+# months = [2200, 2000, 2600, 2130, 2190]
+# jan = months[0]
+# feb = months[1]
+# total = feb-jan
+# print(total)
 
-for i in months:
-    if (i == 2000):
-        print(i)
-    else:
-        print('non')
+# for i in months:
+#     if (i == 2000):
+#         print(i)
+#     else:
+#         print('non')
+numbers = [2, 3, 4, 5, 6, 7, 6]
+print(len(numbers))
