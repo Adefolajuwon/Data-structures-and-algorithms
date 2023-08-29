648596f88a4f760fb0eb4b4b184d5c27cef1571b
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-08-29 10:52:42 +0100

    change

--- problems/double-linked-list.py
+++ problems/double-linked-list.py
@@ -8,3 +8,33 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
+
+    def get_last_node(self):
+        itr = self.head
+        while itr:
+            itr = itr.next
+        return
+
+    def print_forward(self):
+
+        if self.head is None:
+            print('Empty')
+            return
+        itr = self.head
+        llstr = ''
+        while itr:
+            llstr += str(itr.data) + '-->'
+            itr = itr.next
+        print(llstr)
+
+    def print_back(self):
+        if self.tail is None:
+            print('Empty')
+            return
+        itr = self.get_last_node()
+        llstr = ''
+        while itr:
+            llstr += str(itr.data) + '-->'
+            itr = itr.next
+        print(llstr)
