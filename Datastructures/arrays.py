a0995ef69ddee0287b058f7fe4b776228b9d0a5a
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-08-29 12:31:57 +0100

    change

--- Datastructures/arrays.py
+++ Datastructures/arrays.py
@@ -1,16 +1,4 @@
-# # months = [2200, 2000, 2600, 2130, 2190]
-# # jan = months[0]
-# # feb = months[1]
-# # total = feb-jan
-# # print(total)
 
-# # for i in months:
-# #     if (i == 2000):
-# #         print(i)
-# #     else:
-# #         print('non')
-# numbers = [2, 3, 4, 5, 6, 7, 6]
-# print(len(numbers))
 
 nums = [1, 2, 34, 5]
 n = len(nums)
