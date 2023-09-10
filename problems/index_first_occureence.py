1e32bc630502ba22e2b89737e09e370fb2250ad9
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-09-10 15:27:33 +0100

    removed unwanted code

---
+++ problems/index_first_occureence.py
@@ -0,0 +1,10 @@
+
+nums = [2, 7, 11, 15]
+target = 9
+
+for i in range(len(nums)):
+    for j in range(nums[i]):
+        print(nums[i])
+
+nums = [2, 2, 1]
+for i in range:
---
+++ testing.py
@@ -0,0 +1,4 @@
+string_list = ["apple", "banana", "cherry", "date"]
+for i in range(len(string_list)):
+    for j in range(len(string_list[i])):
+        print(j)
