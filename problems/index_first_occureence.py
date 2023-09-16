46f452f1e07d98f7b66f7f1b91347474cd336b8f
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-09-16 13:02:05 +0100

    array

--- problems/index_first_occureence.py
+++ problems/index_first_occureence.py
@@ -1,10 +1,10 @@
 
-nums = [2, 7, 11, 15]
-target = 9
+# nums = [2, 7, 11, 15]
+# target = 9
 
-for i in range(len(nums)):
-    for j in range(nums[i]):
-        print(nums[i])
+# for i in range(len(nums)):
+#     for j in range(nums[i]):
+#         print(nums[i])
 
-nums = [2, 2, 1]
-for i in range:
+# nums = [2, 2, 1]
+# for i in range:
