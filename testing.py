a2db1cab73d1f521153bcf9530b512f75e86de1d
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-09-18 21:49:37 +0100

    array

--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
-string_list = ["apple", "banana", "cherry", "date"]
+string_list = ["aple", "banana", "cherry", "date"]
 for i in range(len(string_list)):
     for j in range(len(string_list[i])):
         print(j)
