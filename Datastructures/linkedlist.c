c1f0864fa66796792d4355fbb0185d9afeec6588
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-12-01 19:52:20 +0100

    it should be s3

---
+++ Datastructures/linkedlist.c
@@ -0,0 +1,12 @@
+typedef struct node {
+    int val;
+    struct node * next;
+} node_t;
+node_t* head = NULL
+head =  (node_t *) malloc(sizeof(node_t)):
+if(head == NULL){
+    return 1
+}
+
+head -> value = 1
+head -> next = NULL
