336e6e4ee4be8d6db753747b0a4e2895a977f08a
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-12-01 22:54:17 +0100

    it should be s3

--- Datastructures/linkedlist.c
+++ Datastructures/linkedlist.c
@@ -1,12 +1,35 @@
 typedef struct node {
-    int val;
-    struct node * next;
+  int val;
+  struct node *next;
 } node_t;
-node_t* head = NULL
-head =  (node_t *) malloc(sizeof(node_t)):
-if(head == NULL){
-    return 1
+
+node_t *head = NULL;
+
+head = (node_t *)malloc(sizeof(node_t));
+
+if (head == NULL) {
+  return 1
 }
 
-head -> value = 1
-head -> next = NULL
+// head -> val = 1
+// head -> next = NULL
+// head =  (node_t *) malloc(sizeof(node_t)):
+// head -> next
+void print_list(node_t *head) {
+  node_t *current = head;
+
+  while (current != NULL) {
+    printf("%d\n", current->val);
+    current = current->next;
+  }
+}
+
+void push(node_t *head, int val) {
+  node_t *current = head;
+  while (current->next != = NULL) {
+    current = current->next;
+  }
+  current->next = (node_t *)malloc(sizeof(node_t));
+  current->val = val;
+  current->next->next = NULL;
+}
