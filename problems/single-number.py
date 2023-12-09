742125dadd38f8ce26b142e0874e9239138b749e
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-12-09 14:56:44 +0100

    seen

--- problems/single-number.py
+++ problems/single-number.py
@@ -1,4 +1,3 @@
-import seen
 # Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
 # You must implement a solution with a linear runtime complexity and use only constant extra space.
@@ -6,11 +5,10 @@ nums = [2, 3, 4, 5]
 
 
 def singleNumber(nums, list):
-            seen = set()
-        for num in nums:
-            if num in seen:
-                seen.remove(num)
-            else:
-                seen.add(num)
-        return seen.pop()
-
+    seen = set()
+    for num in nums:
+        if num in seen:
+            seen.remove(num)
+        else:
+            seen.add(num)
+    return seen.pop()
