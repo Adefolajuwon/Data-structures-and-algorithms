1555f225b7a155b48339f30a68c9af1bcc82bf63
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-12-09 21:47:57 +0100

    quick sort

--- Algorithms/quickSort.py
+++ Algorithms/quickSort.py
@@ -1,20 +1,20 @@
 
-# def partition(elements):
-#     pivot_index = 0
-#     pivot = elements[pivot_index]
-#      start = pivot_index + 1
-#       end = len(elements) - 1
-#        while elements[start] <= pivot:
-#             start += 1
-#         while elements[end] > pivot:
-#             end -= 1
+def partition(elements):
+    pivot_index = 0
+    pivot = elements[pivot_index]
+     start = pivot_index + 1
+      end = len(elements) - 1
+       while elements[start] <= pivot:
+            start += 1
+        while elements[end] > pivot:
+            end -= 1
 
 
-# def quik_sort(elements):
-#     partition(elements)
+def quik_sort(elements):
+    partition(elements)
 
 
-# if __name__ == '__main__':
-#     elements = [11, 9, 29, 7, 2, 15, 28]
-#     quik_sort(elements)
-#     print(elements)
+if __name__ == '__main__':
+    elements = [11, 9, 29, 7, 2, 15, 28]
+    quik_sort(elements)
+    print(elements)
