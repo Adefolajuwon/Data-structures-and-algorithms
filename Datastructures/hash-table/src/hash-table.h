2e2d2cdd4bc1bb821dfb543c1d7d742ca0f90c66
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2024-01-17 15:39:02 +0100

    hash-table

--- Datastructures/hash-table/src/hash-table.h
+++ Datastructures/hash-table/src/hash-table.h
@@ -1,11 +1,15 @@
 // hash_table.h
- struct ht_item {
+struct ht_item {
   char *key;
   char *value;
 };
 
- struct ht_hash_table {
+struct ht_hash_table {
   int size;
   int count;
   ht_item **items;
 };
+// hash_table.h
+void ht_insert(ht_hash_table *ht, const char *key, const char *value);
+char *ht_search(ht_hash_table *ht, const char *key);
+void ht_delete(ht_hash_table *h, const char *key);
