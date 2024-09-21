f642bd5600da9a2e9e5f4237bf0b423f06ecb024
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2024-09-21 12:24:54 +0100

    fuck me

---
+++ problems/two-sum.py
@@ -0,0 +1,23 @@
+'''Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+//example
+input : [2,3,4,6,7]
+target : 7
+'''
+class Solution:
+    def __init__(self):
+        self.nums = nums
+        self.target = target
+
+    def brute():
+        current = 0
+        nextVal = current + 1
+        for num in nums:
+            
+
+
+
---
+++ randoms/double-loops.py
@@ -0,0 +1,6 @@
+nums = [1,2,3,4,5,6,7,8,9]
+for i in  range(len(nums)):
+    for j in range(i + 1, len(nums)):
+      output = nums[j]
+      print(output)
+      
